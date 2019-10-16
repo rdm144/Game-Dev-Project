eb4cbf44d27a46cbc877d24106e1bd1fb718c2ae
rdm144 <46978172+rdm144@users.noreply.github.com>
2019-10-15 20:29:02 -0500

    added background and camera flip

--- Assets/Scripts/Camera/FollowPlayer.cs
+++ Assets/Scripts/Camera/FollowPlayer.cs
@@ -7,14 +7,19 @@ public class FollowPlayer : MonoBehaviour
     GameObject Player;
     Vector3 CameraOffset;
     Vector3 velocity = Vector3.zero;
+    float desiredZ;
+
     // Start is called before the first frame update
     void Start() {
         Player = GameObject.FindGameObjectWithTag("Player");
         CameraOffset = new Vector3(0, 0, -10);
+        desiredZ = 0;
+        StartCoroutine("RotateCamera");
     }
 
     void LateUpdate() {
         SmoothLead();
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, 0, desiredZ), 0.2f);
     }
 
     void SimpleFollow() {
@@ -29,5 +34,21 @@ public class FollowPlayer : MonoBehaviour
         float direction = Mathf.Clamp(Player.GetComponent<Player>().GetVelocity().x, -1, 1);
         Vector3 target = Player.transform.position + CameraOffset + direction * Vector3.right * 8;
         transform.position = Vector3.SmoothDamp(transform.position, target, ref velocity, .3f);
+    }
+
+    IEnumerator RotateCamera()
+    {
+        float waitTimeSeconds = 3;
+        while(true)
+        {
+            yield return new WaitForSeconds(waitTimeSeconds);
+            desiredZ = 90;
+            yield return new WaitForSeconds(waitTimeSeconds);
+            desiredZ = 0;
+            yield return new WaitForSeconds(waitTimeSeconds);
+            desiredZ = -90;
+            yield return new WaitForSeconds(waitTimeSeconds);
+            desiredZ = 180;
+        }
     }
 }
